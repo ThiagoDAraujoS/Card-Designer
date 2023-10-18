e73c55aac44673523c1ef9ed9cdf6a7782f8916b
Thiago de Araujo Silva <ThiagoDAraujoS@Gmail.com>
2023-10-18 06:42:33 -0700

    Added Serialization Deserialization, Clear to BankManager Class
    In the BankManager class, I enhanced the inspect method to perform deserialization of the inspected data bank path file into the inspected_bank variable. Additionally, I introduced a save method to serialize the inspected_bank variable back to the file, facilitating data management and persistence
    Imported jsondataclass to help with serialization and deserialization

--- flask_server/bank_manager.py
+++ flask_server/bank_manager.py
@@ -3,6 +3,8 @@ import shutil
 from os import path
 import json
 from typing import List
+from data import Bank
+import jsondataclass
 
 
 class BankManager:
@@ -10,36 +12,59 @@ class BankManager:
         Banks, in this context, represent directories containing card images and their associated metafiles.
     """
 
-    def __init__(self):
-        self.tool_path: str = ""
+    def __init__(self, main_path):
+        self.MAIN_PATH: str = main_path
         self.existing_bank_names: List[str] = []
         self.inspected_image_bank_path: str = ""
         self.inspected_data_bank_path: str = ""
+        self.inspected_bank: Bank | None = None
+
+    def clear(self):
+        """ Clear the inspected bank """
+        self.inspected_bank = None
+        self.inspected_data_bank_path = ""
+        self.inspected_image_bank_path = ""
+
+    def save(self):
+        """ Save the inspected bank to file """
+        try:
+            with open(self.inspected_data_bank_path, 'w') as data_file:
+                json_string = jsondataclass.to_json(self.inspected_bank)
+                data_file.write(json_string)
+        except Exception as e:
+            print(f"An error occurred while saving the data: {e}")
 
     def inspect(self, bank_name: str) -> None:
         """Set the card data bank file paths based on the selected bank_name."""
-        bank_folder_path = path.join(self.tool_path, bank_name)
+        bank_folder_path = path.join(self.MAIN_PATH, bank_name)
 
         # Construct paths for image and data banks
         self.inspected_image_bank_path = path.join(bank_folder_path, "images")
         self.inspected_data_bank_path = path.join(bank_folder_path, "data.json")
 
-        # Check if the image and data bank paths exist
-        if path.exists(self.inspected_image_bank_path) and path.exists(self.inspected_data_bank_path):
-            print(f"Selected bank: {bank_name}")
-        else:
+        if not path.exists(self.inspected_image_bank_path) or not path.exists(self.inspected_data_bank_path):
             print(f"Bank {bank_name} not found or incomplete.")
+            return
+
+        try:
+            with open(self.inspected_data_bank_path, 'r') as data_file:
+                json_string = data_file.read()
+                self.inspected_bank = jsondataclass.from_json(json_string, Bank)
+        except Exception as e:
+            print(f"An error occurred while loading the data: {e}")
+
+        print(f"Selected bank: {bank_name}")
 
     def load(self) -> None:
         """Read all folders inside self.tool_path and build self.existing_bank_names."""
-        if not (path.exists(self.tool_path) and path.isdir(self.tool_path)):
-            print(f"Tool path {self.tool_path} does not exist or is not a directory.")
+        if not (path.exists(self.MAIN_PATH) and path.isdir(self.MAIN_PATH)):
+            print(f"Tool path {self.MAIN_PATH} does not exist or is not a directory.")
             return
-        self.existing_bank_names = [name for name in os.listdir(self.tool_path) if path.isdir(path.join(self.tool_path, name))]
+        self.existing_bank_names = [name for name in os.listdir(self.MAIN_PATH) if path.isdir(path.join(self.MAIN_PATH, name))]
 
     def create(self, name: str) -> None:
         """Create a new card bank with the given name."""
-        bank_folder_path = path.join(self.tool_path, name)
+        bank_folder_path = path.join(self.MAIN_PATH, name)
 
         if path.exists(bank_folder_path):
             print(f"Bank {name} already exists.")
@@ -62,8 +87,8 @@ class BankManager:
 
     def copy(self, source_bank_name: str, new_bank_name: str) -> None:
         """Copy an existing card bank and change its name."""
-        source_folder_path = path.join(self.tool_path, source_bank_name)
-        destination_folder_path = path.join(self.tool_path, new_bank_name)
+        source_folder_path = path.join(self.MAIN_PATH, source_bank_name)
+        destination_folder_path = path.join(self.MAIN_PATH, new_bank_name)
 
         if not path.exists(source_folder_path) or path.exists(destination_folder_path):
             print("Source bank does not exist or the destination bank already exists.")
@@ -78,7 +103,7 @@ class BankManager:
 
     def delete(self, bank_name: str) -> None:
         """Delete an existing card bank."""
-        bank_folder_path = path.join(self.tool_path, bank_name)
+        bank_folder_path = path.join(self.MAIN_PATH, bank_name)
 
         if not path.exists(bank_folder_path) or not path.isdir(bank_folder_path):
             print(f"Bank '{bank_name}' does not exist or is not a directory.")
