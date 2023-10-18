40c9ac2b8736c90a5a23ac93e8689a5902b24c9c
Thiago de Araujo Silva <ThiagoDAraujoS@Gmail.com>
2023-10-18 06:45:19 -0700

    Refactored ImageFolder Class: Renamed variable from 'folder_path' to 'path'
    This commit includes a refactor of the ImageFolder class. I've renamed the attribute 'folder_path' to 'path' for improved clarity and consistency in the class.

--- flask_server/image_folder.py
+++ flask_server/image_folder.py
@@ -6,7 +6,7 @@ class ImageFolder:
 
     def __init__(self):
         """ Initialize an ImageFolder instance with an empty folder path. """
-        self.folder_path = ""
+        self.path = ""
 
     def set_image_folder(self, folder_path: str) -> None:
         """ Set the image folder path if it exists. """
@@ -14,4 +14,4 @@ class ImageFolder:
             print(f"Image folder '{folder_path}' does not exist or is not a directory.")
             return
 
-        self.folder_path = folder_path
+        self.path = folder_path
