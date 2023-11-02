5d90193de4edc6d75b951525d89cfb2157cb411b
Thiago de Araujo Silva <ThiagoDAraujoS@Gmail.com>
2023-11-01 20:11:13 -0700

    Added Image class
    This class manages images contained in the image bank

--- flask_server/app/__init__.py
+++
@@ -1,6 +0,0 @@
-from typing import NewType
-
-UUIDString = NewType('UUIDString', str)
-JsonString = NewType('JsonString', str)
-TypeString = NewType('TypeString', str)
-PathString = NewType('PathString', str)
